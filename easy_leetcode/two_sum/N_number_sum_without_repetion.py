5964e722fd5cb22f91be919746076ee39ffa54e4
Allu Prasad <122075992+allu4prasad@users.noreply.github.com>
2023-01-09 15:27:33 +0530

    Create N_number_sum_without_repetion.py

---
+++ easy_leetcode/two_sum/N_number_sum_without_repetion.py
@@ -0,0 +1,11 @@
+def find_N_number_sum_combination(nums,target,partial=[]):
+    if sum(partial)==target:
+        combination.append(partial)
+    for index,num in enumerate(nums):
+        remaining=nums[index+1:]
+        if num not in partial:
+            find_N_number_sum_combination(remaining,target,partial+[num])
+
+combination = []
+find_N_number_sum_combination([1, 2, 3, 4,6,5,7, 5], 10)
+print(combination)
