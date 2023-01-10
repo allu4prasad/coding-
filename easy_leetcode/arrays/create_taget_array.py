6e37883a6e9867c338301f9f406bad6de3b25bb2
Allu Prasad <122075992+allu4prasad@users.noreply.github.com>
2023-01-10 07:27:04 +0530

    Update create_taget_array.py

--- easy_leetcode/arrays/create_taget_array.py
+++ easy_leetcode/arrays/create_taget_array.py
@@ -1,5 +1,18 @@
+#1
 def create_target_array(nums, index):
     target = []
     for i in range(len(nums)):
         target.insert(index[i], nums[i])
     return target
+#2
+class Solution(object):
+    def createTargetArray(self, nums, index):
+        """
+        :type nums: List[int]
+        :type index: List[int]
+        :rtype: List[int]
+        """
+        results=[]
+        for i,j in zip(index,nums):
+            results[i:i]=[j]
+        return results
