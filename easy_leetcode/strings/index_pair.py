48b33524dc95faf6e52e4c995f8d4b5b31faeaf7
Allu Prasad <122075992+allu4prasad@users.noreply.github.com>
2023-01-17 08:51:43 +0530

    Update index_pair.py

--- easy_leetcode/strings/index_pair.py
+++ easy_leetcode/strings/index_pair.py
@@ -1,1 +1,13 @@
 
+class Solution(object):
+    def indexPairs(self, text, words):
+        results=[]
+        for i in words:
+            start=0
+            while start<len(text):
+                start=text.find(i,start)
+                if start==-1:
+                    break
+                results.append([start,start+len(i)-1])
+                start+=1
+        return (sorted(results))
